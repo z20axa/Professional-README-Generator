b5ede6a635f99b6fea37a358226fa02357e69c25
z20axa <alejandrospan@hotmail.com>
2022-12-30 18:45:26 -0500

    Adds module exports in generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,13 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}`;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  renderLicenseBadge,
+  renderLicenseLink,
+  renderLicenseSection,
+  generateMarkdown
+};
+
