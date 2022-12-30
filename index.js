09427f8e51de147f0e1bd6adc903c126f02e7a87
z20axa <alejandrospan@hotmail.com>
2022-12-30 18:50:18 -0500

    Adds initial functions codes for both JS files

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const inquirer = require('inquirer');
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+};
 
 // Function call to initialize app
 init();
