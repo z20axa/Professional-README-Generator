0f271d1d8f43a547bdf865b7b0c69580b408be39
z20axa <alejandrospan@hotmail.com>
2023-01-26 15:11:06 -0500

    Adds comments to the generate markdown JS file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,19 @@
 // packages required
 const { writeFile } = require('fs');
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-
-};
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-
-};
-
-// TODO: Create a function to generate markdown for README
+// function declaration to generate markdown for README
 function generateMarkdown(data) {
-  let readme = `# ${data.projectTitle}
-  <!-- adding license badge near the top of the README -->
+  let readme = `
+  <!-- adding project tittle from user entered input data -->
+  # ${data.projectTitle}
+
+  <!-- adding license badge from user selected input data near the top of the README -->
   <!-- condition to check for the project license to display the license badge, if not license, then return an empty string -->
   ${data.projectLicense == "None" ? "" : `![License Badge](https://img.shields.io/badge/License-${data.projectLicense}-blue)`}
 
 
   ## Description
-
+  <!-- adding project description from user entered input data -->
   ${data.projectDescription}
 
   ## Table of Contents
@@ -33,11 +24,11 @@ function generateMarkdown(data) {
   - [License](#license)
   
   ## Installation
-
+  <!-- adding project installation instructions from user entered input data -->
   ${data.projectInstallation}
          
   ## Usage
-
+  <!-- adding project usage from user entered input data -->
   ${data.projectUsage}
   
   Provide instructions and examples for use. Include screenshots as needed.
@@ -45,7 +36,7 @@ function generateMarkdown(data) {
   ![alt text](assets/images/screenshot.png)
 
   ## License
-
+  <!-- adding which license the application is covered under from user entered input data -->
   ${data.projectLicense}
   
   The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
@@ -55,7 +46,7 @@ function generateMarkdown(data) {
   🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
   
   ## Contributing
-
+  <!-- adding project contributions from user entered input data -->
   ${data.projectContributions}
   
   ## Badges
@@ -65,17 +56,18 @@ function generateMarkdown(data) {
   Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
   
   ## Tests
-
+  <!-- adding project tests from user entered input data -->
   ${data.projectInstructions}
 
 
   ## Questions
-
+  <!-- adding user GitHib user name from user entered input data -->
   ${data.githubUserName}
+  <!-- adding user email from user entered input data -->
   ${data.userEmail}
 
   `;
-
+  // function call to write to the file 
   writeFile("./README2.md", readme, (err) => {
     if (err) return console.error(err);
     console.log("Success!");
